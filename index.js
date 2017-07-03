9c25989fc6d6463dcc6a0f81594e4f27cc1c07ab
sustny <sustny@gmail.com>
2017-07-03 17:34:07 +0900

    Array

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // Created by sustny(http://sustny.me/)
 //
 
+/*
 var sumNum = function(n1, n2, n3) {
   var sum = n1 + n2 + n3;
   return sum;
@@ -12,5 +13,18 @@ var sumNum = function(n1, n2, n3) {
 var total = sumNum(1, 3, 5);
 console.log(total);
 
-var message = "Hello world!"
-console.log(message);
+var msg = "Global";
+
+var message = function() {
+  var msg = "Private";
+  console.log(msg);
+}
+
+console.log(msg);
+message();
+*/
+
+var array1 = new Array(3);
+var array2 = [3];
+
+console.log("1: " + array1 + " 2: " + array2);
