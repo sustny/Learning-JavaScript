c7a08d1877ef0b7a9d8652343a12c7e5cc266736
sustny <sustny@gmail.com>
2017-07-03 16:49:08 +0900

    sum

---
+++ index.js
@@ -0,0 +1,13 @@
+//
+// index.js
+// Created on 2017-07-03 16:40
+// Created by sustny(http://sustny.me/)
+//
+
+function sumNum(n1, n2, n3) {
+  var sum = n1+n2+n3;
+  return sum;
+}
+
+var total = sumNum(1, 3, 5);
+alert(total);
